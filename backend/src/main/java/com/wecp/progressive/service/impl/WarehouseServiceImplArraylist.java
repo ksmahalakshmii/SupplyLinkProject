77125747893406874a84806d84f98052fe755c72
username <wecp-dummy@gmail.com>
2025-01-02 13:10:10 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplArraylist.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplArraylist.java
@@ -1,22 +1,18 @@
 package com.wecp.progressive.service.impl;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import com.wecp.progressive.entity.Supplier;
 import com.wecp.progressive.entity.Warehouse;
 import com.wecp.progressive.service.WarehouseService;
+import org.springframework.stereotype.Service;
 
-public class WarehouseServiceImplArraylist implements WarehouseService
-{
-    List<Warehouse> warehouseList=new ArrayList<>();
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
-    @Override
-    public int addWarehouse(Warehouse warehouse) 
-    {
-        warehouseList.add(warehouse);
-        return warehouse.getWarehouseId();
-    }
+@Service
+public class WarehouseServiceImplArraylist implements WarehouseService {
+
+    private static List<Warehouse> warehouseList = new ArrayList<>();
 
     @Override
     public List<Warehouse> getAllWarehouses() {
@@ -24,16 +20,20 @@ public class WarehouseServiceImplArraylist implements WarehouseService
     }
 
     @Override
-    public List<Warehouse> getWarehousesSortedByCapacity() 
-    {
-        Collections.sort(warehouseList);
-        return warehouseList;
+    public int addWarehouse(Warehouse warehouse) {
+        warehouseList.add(warehouse);
+        return warehouseList.size();
     }
 
     @Override
-    public void emptyArrayList()
-    {
-        warehouseList=new ArrayList<>();
+    public List<Warehouse> getWarehousesSortedByCapacity() {
+        List<Warehouse> sortedWarehouses = warehouseList;
+        sortedWarehouses.sort(Comparator.comparing(Warehouse::getCapacity)); // Sort by supplier name
+        return sortedWarehouses;
     }
 
+    @Override
+    public void emptyArrayList() {
+        warehouseList = new ArrayList<>();
+    }
 }
